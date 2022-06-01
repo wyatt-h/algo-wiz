6372b094509c07aea5b41f168d64fc04ecaf59ed
wyatt-h <wyatt.hskr@gmail.com>
2022-05-31 21:28:27 -0400

    refine merge sort algorithm to save space complexity

--- src/hooks/useMergeSort.js
+++ src/hooks/useMergeSort.js
@@ -1,98 +1,55 @@
-import { sleep, swapHeight } from "../utilities";
+import { sleep } from "../utilities";
 
-const merge = async (arr, arrL, arrR, start) => {
-  console.log("merge");
-  if (arrL.length === 0) {
-    return arrR;
+const merge = async (arr, helper, low, mid, high, duration) => {
+  for (let i = low; i <= high; i++) {
+    helper[i] = arr[i].offsetHeight;
   }
-  if (arrR.length === 0) {
-    return arrL;
-  }
-  let pointerL = 0;
-  let pointerR = 0;
-  let result = [];
-  let index = start;
-  let temp_height;
-  console.log("arrL: ", arrL, ", arrR: ", arrR);
-  while (pointerL < arrL.length || pointerR < arrR.length) {
-    console.log(arrL[pointerL]);
-    console.log(arrR[pointerR]);
-    if (pointerR === arrR.length) {
-      temp_height = arrL[pointerL].offsetHeight;
-      result.push(arrL[pointerL++]);
-      // arr[index++].style.height = arrL[pointerL++].offsetHeight;
-    } else if (pointerL === arrL.length) {
-      temp_height = arrR[pointerR].offsetHeight;
-      result.push(arrR[pointerR++]);
-      // arr[index++].style.height = arrR[pointerR++].offsetHeight;
-    } else if (arrL[pointerL].offsetHeight < arrR[pointerR].offsetHeight) {
-      temp_height = arrL[pointerL].offsetHeight;
-      result.push(arrL[pointerL++]);
-      // arr[index++].style.height = arrL[pointerL++].offsetHeight;
+
+  let helperL = low;
+  let helperR = mid + 1;
+  let curr = low;
+  while (helperL <= mid && helperR <= high) {
+    arr[curr].classList.add("on-sorted");
+    await sleep(duration);
+    arr[curr].classList.remove("on-sorted");
+    if (helper[helperL] <= helper[helperR]) {
+      arr[curr++].style.height = `${helper[helperL++]}px`;
     } else {
-      temp_height = arrR[pointerR].offsetHeight;
-      result.push(arrR[pointerR++]);
-      // arr[index++].style.height = arrR[pointerR++].offsetHeight;
+      arr[curr++].style.height = `${helper[helperR++]}px`;
     }
-    arr[index].classList.add("on-sorted");
-    await sleep(1000);
-    arr[index].classList.remove("on-sorted");
-    arr[index++].style.height = `${temp_height}px`;
-    console.log(result);
-    console.log(pointerL, pointerR);
-    console.log(arrL.length, arrR.length);
   }
-  return result;
+
+  let remaining = mid - helperL;
+  for (let i = 0; i <= remaining; i++) {
+    arr[curr].classList.add("on-sorted");
+    await sleep(duration);
+    arr[curr + i].style.height = `${helper[helperL + i]}px`;
+    arr[curr].classList.remove("on-sorted");
+  }
 };
 
-const mergeSortHelper = async (arr, start, end) => {
-  console.log("split");
-  if (end - start <= 1) {
-    return arr.slice(start, end);
+const mergeSortHelper = async (arr, helper, low, high, duration) => {
+  if (low < high) {
+    let mid = parseInt((low + high) / 2);
+    await mergeSortHelper(arr, helper, low, mid);
+    await mergeSortHelper(arr, helper, mid + 1, high);
+    await merge(arr, helper, low, mid, high, duration);
   }
-  const mid = parseInt((start + end) / 2);
-  console.log("one more layer");
-  console.log("left: ", start, ", mid: ", mid, ", right: ", end);
-  console.log(arr.slice(start, mid));
-  console.log(arr.slice(mid, end));
-
-  const result = await merge(
-    arr,
-    await mergeSortHelper(arr, start, mid),
-    await mergeSortHelper(arr, mid, end),
-    start
-  );
-  return result;
 };
 
 const useMergeSort = async (duration) => {
-  // const arr = [4, 6, 8, 2, 5, 9, 23, 1, 17, 88];
-  // const result = mergeSortHelper(arr, 0, arr.length);
-  // console.log(result);
-  // return result;
   const sticks = document.querySelectorAll(".stick");
   const sortBtn = document.querySelector(".sort-btn");
+  let helper = new Array(sticks.length);
+  helper.fill(undefined);
   await Array.from(sortBtn.classList).includes("on-sorting");
-  const result = await mergeSortHelper(Array.from(sticks), 0, sticks.length);
-  result.forEach((stick) => console.log(stick.offsetHeight));
-  console.log(result);
-  return result;
-  // for (let i = 0; i < sticks.length; i++) {
-  //   sticks[i].classList.add("on-sorted");
-  //   let height_i = sticks[i].offsetHeight;
-  //   for (let j = i + 1; j < sticks.length; j++) {
-  //     if (!Array.from(sortBtn.classList).includes("on-sorting")) break;
-  //     let height_j = sticks[j].offsetHeight;
-  //     sticks[j].classList.add("on-compared");
-  //     if (height_i > height_j) {
-  //       swapHeight(sticks[i], sticks[j]);
-  //       height_i = sticks[i].offsetHeight;
-  //     }
-  //     await sleep(duration);
-  //     sticks[j].classList.remove("on-compared");
-  //   }
-  //   sticks[i].classList.remove("on-sorted");
-  // }
+  await mergeSortHelper(
+    Array.from(sticks),
+    helper,
+    0,
+    sticks.length - 1,
+    duration
+  );
 };
 
 export default useMergeSort;
